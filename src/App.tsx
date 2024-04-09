4890c8caa6a8e3cabed04e5639279df42953d2e1
sangyeon Lee <asylee02@gmail.com>
2024-04-09 14:39:04 +0900

    [style] 기본 페이지 크기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return(
-    <>
-      <div className='text-2xl'>hello FatPet Project</div>
-    </>
+    <div className='max-w-[480px] bg-gray-200 w-auto min-h-full'>
+      <div className='text-2xl w-[440px]'>FatPet Project</div>
+    </div>
   )
 }
 

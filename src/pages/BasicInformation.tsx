73f7cfb5a6975d05bd34a200382aee4b75c99156
sangyeon Lee <asylee02@gmail.com>
2024-04-26 15:51:23 +0900

    [chore] 기본 세팅(#4)

--- src/pages/BasicInformation.tsx
+++ src/pages/BasicInformation.tsx
@@ -1,8 +1,21 @@
+import Form from "@/components/Form";
 
 export default function BasicInformation() {
+
+    function handleSumbit(){
+        return
+    }
+
     return (
         <div>
-            기본입력받기
+            <p>반려동물의 기본정보를 입력해주세요</p>
+            <Form onSubmit={handleSumbit}>
+                <Form.Input name="이름" type="text"/>
+                <div className="flex ">
+                    <Form.Input name="나이" type="text"/>
+                    <Form.Input name="성별" type="text"/>
+                </div>
+            </Form>
         </div>
     );
 }

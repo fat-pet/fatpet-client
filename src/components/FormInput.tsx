bc453eb0c320a5b82573ab1a7289911168accc1d
sangyeon <asylee02@gmail.com>
2024-04-28 23:29:05 +0900

    [chore] 컴포넌트 props 수정(#4)

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -3,7 +3,7 @@ import { useFormContext } from "../contexts/FormContext";
 
 interface FormInputProps{
     name : string;
-    type? : "text" | "password" | "email";
+    type? : "text" | "password" | "email" | "number";
     minlen ?: number;
     maxlen ?: number;
 }
@@ -19,13 +19,12 @@ const FormInput :React.FC<FormInputProps> = ({name, type="text", minlen, maxlen}
                     <span className="text-sm text-red-500 pt-2">
                         { errors[name]?.message as string}
                     </span>}
-
                 </div>
                 <input 
                 id={name}
                 type={type}
                 {...register(name, {
-                    required: `${name}은(는) 필수 입력입니다.`,
+                    required: `${name}을(를) 입력해주세요.`,
                     pattern: {
                         value: new RegExp(name==='이메일' ? "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" : "^[A-Za-z0-9]*$"),
                         message: "띄어쓰기를 사용할 수 없습니다."
@@ -33,7 +32,8 @@ const FormInput :React.FC<FormInputProps> = ({name, type="text", minlen, maxlen}
                     ...(minlen && { minLength: { value: minlen, message: `${name}는 ${minlen}글자 이상이어야 합니다.` }}),
                     ...(maxlen && { maxLength: { value: maxlen, message: `${name}는 ${maxlen}글자 이하이어야 합니다.` }}),
                   })}
-                className="w-[5/6] mt-3 h-12 bg-gray-50 border-2 border-gray-200"
+                className={`w-full mt-3 h-12 bg-gray-50 border-2 border-gray-200`}
+
                 />
                 
             </label>
--- src/pages/BasicInformation.tsx
+++ src/pages/BasicInformation.tsx
@@ -1,20 +1,34 @@
 import Form from "@/components/Form";
-
+import { FaDog } from "react-icons/fa6";
+import { FaCat } from "react-icons/fa";
 export default function BasicInformation() {
 
     function handleSumbit(){
         return
     }
+    function handleClick(){
+        console.log('hello')   
+    }
 
     return (
-        <div>
-            <p>반려동물의 기본정보를 입력해주세요</p>
+        <div className="flex flex-col items-center mt-20 mx-8">
+            <p className="text-xl font-bold tracking-tighter mb-10">반려동물의 기본정보를 입력해주세요</p>
             <Form onSubmit={handleSumbit}>
                 <Form.Input name="이름" type="text"/>
-                <div className="flex ">
-                    <Form.Input name="나이" type="text"/>
-                    <Form.Input name="성별" type="text"/>
+                <div className="flex mt-5">
+                    <div className="w-1/3 flex mr-10">
+                        <Form.Input name="나이" type="number"/>
+                        <p className="font-bold text-lg mt-12 ml-5">살</p>
+                    </div>
+                    <div className="">
+                        <p className="font-bold text-lg ">종류</p>
+                        <div className="flex mt-4 font-bold text-lg">
+                            <button className="border-2 rounded-lg w-28 h-10 mr-5 flex items-center justify-center hover:bg-gray-300" onClick={handleClick}><FaDog className="w-7 h-7 mr-2"/>강아지</button>
+                            <button className="border-2 rounded-lg w-28 h-10 mr-5 flex items-center justify-center hover:bg-gray-300" onClick={handleClick}><FaCat className="w-7 h-7 mr-2"/>고양이</button>
+                        </div>
+                    </div>
                 </div>
+
             </Form>
         </div>
     );

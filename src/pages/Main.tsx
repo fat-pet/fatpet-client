f7ac10bb4ef91aac61d1d0eafed32464f2d02f61
asylee02 <87488288+asylee02@users.noreply.github.com>
2024-05-26 21:55:48 +0900

    [release] 3차 배포 에러 수정-

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import Header from '@/components/Header';
 import Form from '../components/Form';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
   const navigate = useNavigate();

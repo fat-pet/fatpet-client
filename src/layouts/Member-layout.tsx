cf7759a36e5d14c7da8114b5917452c350e72df0
sangyeon Lee <asylee02@gmail.com>
2024-05-30 13:44:36 +0900

    [design] 결과 그래프 상태에 따라 data값 변경

--- src/layouts/Member-layout.tsx
+++ src/layouts/Member-layout.tsx
@@ -6,7 +6,7 @@ import WithAuth from '@/utils/WithAuth';
 const MemberLayout = () => {
   return (
     <div className="max-w-[480px] shadow w-full h-full ">
-      <div className="h-layout-main pt-14 px-2 overflow-auto scrollbar-hidden">
+      <div className="h-layout-main px-2 overflow-auto scrollbar-hidden">
         <Outlet />
       </div>
       <div className="h-layout-footer">
